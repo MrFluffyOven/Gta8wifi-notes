54874c9b8ba6d7b162fec50bdd18aa650a6e988b
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-23 20:14:08 +0100

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,44 +1,46 @@
- 259       20       1024 mmcblk0p27
- 259       21       1024 mmcblk0p28
- 259       22       1024 mmcblk0p29
- 259       23      16384 mmcblk0p30
- 259       24       6144 mmcblk0p31
- 259       25       4096 mmcblk0p32
- 259       26      17408 mmcblk0p33
- 259       27      10240 mmcblk0p34
- 259       28       1024 mmcblk0p35
- 259       29      10240 mmcblk0p36
- 259       30      65536 mmcblk0p37
- 259       31      65536 mmcblk0p38
- 259       32       8192 mmcblk0p39
- 259       33      76800 mmcblk0p40
- 259       34      24576 mmcblk0p41
- 259       35      16384 mmcblk0p42
- 259       36       1024 mmcblk0p43
- 259       37       1024 mmcblk0p44
- 259       38       6144 mmcblk0p45
- 259       39    6582272 mmcblk0p46
- 259       40     425984 mmcblk0p47
- 259       41      20480 mmcblk0p48
- 259       42     204800 mmcblk0p49
- 259       43      24576 mmcblk0p50
- 259       44      10240 mmcblk0p51
- 259       45       6144 mmcblk0p52
- 259       46   22815724 mmcblk0p53 /Userdata
- 179       16       4096 mmcblk0boot1
- 179        8       4096 mmcblk0boot0
- 179       24   30228480 mmcblk1
- 179       25   30227439 mmcblk1p1
- 253        0    3755296 dm-0 /System
- 253        1     325616 dm-1
- 253        2     782364 dm-2
- 253        3    1224232 dm-3
- 253        4    3695960 dm-4
- 253        5     769932 dm-5
- 253        6    1204832 dm-6
- 253        7     419184 dm-7
- 253        8      20076 dm-8
- 253       21       8452 dm-21
- 253       27       2912 dm-27
- 253       43   22815724 dm-43 /Data
- 253       44      75508 dm-44
+<?>       <?>      <Blocks>
+ 
+ 259       20       1024      mmcblk0p27
+ 259       21       1024      mmcblk0p28
+ 259       22       1024      mmcblk0p29
+ 259       23      16384      mmcblk0p30
+ 259       24       6144      mmcblk0p31
+ 259       25       4096      mmcblk0p32
+ 259       26      17408      mmcblk0p33
+ 259       27      10240      mmcblk0p34
+ 259       28       1024      mmcblk0p35
+ 259       29      10240      mmcblk0p36
+ 259       30      65536      mmcblk0p37
+ 259       31      65536      mmcblk0p38
+ 259       32       8192      mmcblk0p39
+ 259       33      76800      mmcblk0p40
+ 259       34      24576      mmcblk0p41
+ 259       35      16384      mmcblk0p42
+ 259       36       1024      mmcblk0p43
+ 259       37       1024      mmcblk0p44
+ 259       38       6144      mmcblk0p45
+ 259       39    6582272      mmcblk0p46
+ 259       40     425984      mmcblk0p47
+ 259       41      20480      mmcblk0p48
+ 259       42     204800      mmcblk0p49
+ 259       43      24576      mmcblk0p50
+ 259       44      10240      mmcblk0p51
+ 259       45       6144      mmcblk0p52
+ 259       46   22815724      mmcblk0p53 /userdata
+ 179       16       4096      mmcblk0boot1
+ 179        8       4096      mmcblk0boot0
+ 179       24   30228480      mmcblk1
+ 179       25   30227439      mmcblk1p1
+ 253        0    3755296      dm-0 /system
+ 253        1     325616      dm-1
+ 253        2     782364      dm-2
+ 253        3    1224232      dm-3
+ 253        4    3695960      dm-4
+ 253        5     769932      dm-5
+ 253        6    1204832      dm-6
+ 253        7     419184      dm-7
+ 253        8      20076      dm-8
+ 253       21       8452      dm-21
+ 253       27       2912      dm-27
+ 253       43   22815724      dm-43 /data
+ 253       44      75508      dm-44
