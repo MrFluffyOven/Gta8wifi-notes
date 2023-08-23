322efabb66f837849258bde036db1bff0b4a0e0f
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-23 22:06:24 +0100

    Update parted.mk

--- parted.mk
+++ parted.mk
@@ -1,54 +1,54 @@
-Partition       Size    File system  Name           Flags
- mmc/blk/0p1    10.5MB  ext4         prodnv         msftdata
- mmc/blk/0p2    1049kB               miscdata       msftdata
- mmc/blk/0p3    2097kB               l_fixnv1       msftdata
- mmc/blk/0p4    2097kB               l_fixnv2       msftdata
- mmc/blk/0p5    1049kB               l_deltanv      msftdata
- mmc/blk/0p6    2097kB               l_runtimenv1   msftdata
- mmc/blk/0p7    2097kB               l_runtimenv2   msftdata
- mmc/blk/0p8    1049kB               misc           msftdata
- mmc/blk/0p9    10.5MB               param          msftdata
-mmc/blk/0p10    16.8MB  ext4         vendor_efs     msftdata
-mmc/blk/0p11    16.8MB  ext4         sec_efs        msftdata
-mmc/blk/0p12    8389kB  ext4         persist        msftdata
-mmc/blk/0p13    4194kB               steady         msftdata
-mmc/blk/0p14    1049kB               persistent     msftdata
-mmc/blk/0p15    3146kB               pad            msftdata
-mmc/blk/0p16    6291kB               trustos        msftdata
-mmc/blk/0p17    6291kB               trustos_bak    msftdata
-mmc/blk/0p18    1049kB               sml            msftdata
-mmc/blk/0p19    1049kB               sml_bak        msftdata
-mmc/blk/0p20    2097kB               pm_sys         msftdata
-mmc/blk/0p21    1049kB               teecfg         msftdata
-mmc/blk/0p22    1049kB               teecfg_bak     msftdata
-mmc/blk/0p23    4194kB               tzar           msftdata
-mmc/blk/0p24    5243kB               uboot          msftdata
-mmc/blk/0p25    1049kB               uboot_bak      msftdata
-mmc/blk/0p26    1049kB               vbmeta         msftdata
-mmc/blk/0p27    1049kB               vbmeta_bak     msftdata
-mmc/blk/0p28    1049kB               grd_fw         msftdata
-mmc/blk/0p29    1049kB               btd            msftdata
-mmc/blk/0p30    16.8MB               uboot_log      msftdata
-mmc/blk/0p31    6291kB               logo           msftdata
-mmc/blk/0p32    4194kB               fbootlogo      msftdata
-mmc/blk/0p33    17.8MB               bota           msftdata
-mmc/blk/0p34    10.5MB               debug          msftdata
-mmc/blk/0p35    1049kB               gnssmodem      msftdata
-mmc/blk/0p36    10.5MB               wcnmodem       msftdata
-mmc/blk/0p37    67.1MB               boot           msftdata
-mmc/blk/0p38    67.1MB               recovery       msftdata
-mmc/blk/0p39    8389kB               dtbo           msftdata
-mmc/blk/0p40    78.6MB  ext2         socko          msftdata
-mmc/blk/0p41    25.2MB               odmko          msftdata
-mmc/blk/0p42    16.8MB  ext4         metadata       msftdata
-mmc/blk/0p43    1049kB               vbmeta_system  msftdata
-mmc/blk/0p44    1049kB               vbmeta_vendor  msftdata
-mmc/blk/0p45    6291kB               pad2           msftdata
-mmc/blk/0p46    6740MB               super          msftdata
-mmc/blk/0p47    436MB   ext2         prism          msftdata
-mmc/blk/0p48    21.0MB  ext2         optics         msftdata
-mmc/blk/0p49    210MB   ext4         cache          msftdata
-mmc/blk/0p50    25.2MB  ext4         omr            msftdata
-mmc/blk/0p51    10.5MB               sysdumpdb      msftdata
-mmc/blk/0p52    6291kB               l_agdsp        msftdata
-mmc/blk/0p53    23.4GB               userdata       msftdata
+ <Partition>    <Size>  <fs>  <Name>           Flags
+ mmc/blk/0p1    10.5MB  ext4   prodnv         msftdata
+ mmc/blk/0p2    1049kB         miscdata       msftdata
+ mmc/blk/0p3    2097kB         l_fixnv1       msftdata
+ mmc/blk/0p4    2097kB         l_fixnv2       msftdata
+ mmc/blk/0p5    1049kB         l_deltanv      msftdata
+ mmc/blk/0p6    2097kB         l_runtimenv1   msftdata
+ mmc/blk/0p7    2097kB         l_runtimenv2   msftdata
+ mmc/blk/0p8    1049kB         misc           msftdata
+ mmc/blk/0p9    10.5MB         param          msftdata
+mmc/blk/0p10    16.8MB  ext4   vendor_efs     msftdata
+mmc/blk/0p11    16.8MB  ext4   sec_efs        msftdata
+mmc/blk/0p12    8389kB  ext4   persist        msftdata
+mmc/blk/0p13    4194kB         steady         msftdata
+mmc/blk/0p14    1049kB         persistent     msftdata
+mmc/blk/0p15    3146kB         pad            msftdata
+mmc/blk/0p16    6291kB         trustos        msftdata
+mmc/blk/0p17    6291kB         trustos_bak    msftdata
+mmc/blk/0p18    1049kB         sml            msftdata
+mmc/blk/0p19    1049kB         sml_bak        msftdata
+mmc/blk/0p20    2097kB         pm_sys         msftdata
+mmc/blk/0p21    1049kB         teecfg         msftdata
+mmc/blk/0p22    1049kB         teecfg_bak     msftdata
+mmc/blk/0p23    4194kB         tzar           msftdata
+mmc/blk/0p24    5243kB         uboot          msftdata
+mmc/blk/0p25    1049kB         uboot_bak      msftdata
+mmc/blk/0p26    1049kB         vbmeta         msftdata
+mmc/blk/0p27    1049kB         vbmeta_bak     msftdata
+mmc/blk/0p28    1049kB         grd_fw         msftdata
+mmc/blk/0p29    1049kB         btd            msftdata
+mmc/blk/0p30    16.8MB         uboot_log      msftdata
+mmc/blk/0p31    6291kB         logo           msftdata
+mmc/blk/0p32    4194kB         fbootlogo      msftdata
+mmc/blk/0p33    17.8MB         bota           msftdata
+mmc/blk/0p34    10.5MB         debug          msftdata
+mmc/blk/0p35    1049kB         gnssmodem      msftdata
+mmc/blk/0p36    10.5MB         wcnmodem       msftdata
+mmc/blk/0p37    67.1MB         boot           msftdata
+mmc/blk/0p38    67.1MB         recovery       msftdata
+mmc/blk/0p39    8389kB         dtbo           msftdata
+mmc/blk/0p40    78.6MB  ext2   socko          msftdata
+mmc/blk/0p41    25.2MB         odmko          msftdata
+mmc/blk/0p42    16.8MB  ext4   metadata       msftdata
+mmc/blk/0p43    1049kB         vbmeta_system  msftdata
+mmc/blk/0p44    1049kB         vbmeta_vendor  msftdata
+mmc/blk/0p45    6291kB         pad2           msftdata
+mmc/blk/0p46    6740MB         super          msftdata
+mmc/blk/0p47    436MB   ext2   prism          msftdata
+mmc/blk/0p48    21.0MB  ext2   optics         msftdata
+mmc/blk/0p49    210MB   ext4   cache          msftdata
+mmc/blk/0p50    25.2MB  ext4   omr            msftdata
+mmc/blk/0p51    10.5MB         sysdumpdb      msftdata
+mmc/blk/0p52    6291kB         l_agdsp        msftdata
+mmc/blk/0p53    23.4GB         userdata       msftdata
