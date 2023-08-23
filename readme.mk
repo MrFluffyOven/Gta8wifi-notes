40803b5bcf9714cf3d5936d838914d6437f1e876
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-23 20:34:35 +0100

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,4 +1,4 @@
-<?>       <?>   <Blocks>     <Partiton>
+<?>       <?>   <Blocks>     <Partiton>      <Mount>
  179        0   30535680      mmcblk0
  179        1      10240      mmcblk0p1
  179        2       1024      mmcblk0p2
@@ -7,52 +7,52 @@
  179        5       1024      mmcblk0p5
  179        6       2048      mmcblk0p6
  179        7       2048      mmcblk0p7
- 259        1       1024      mmcblk0p8 /misc
+ 259        1       1024      mmcblk0p8       /misc
  259        2      10240      mmcblk0p9
  259        3      16384      mmcblk0p10
  259        4      16384      mmcblk0p11
- 259        5       8192      mmcblk0p12 /persist
+ 259        5       8192      mmcblk0p12      /persist
  259        6       4096      mmcblk0p13
  259        7       1024      mmcblk0p14
  259        8       3072      mmcblk0p15
- 259        9       6144      mmcblk0p16 /tos
+ 259        9       6144      mmcblk0p16      /tos
  259       10       6144      mmcblk0p17
- 259       11       1024      mmcblk0p18 /sml
+ 259       11       1024      mmcblk0p18      /sml
  259       12       1024      mmcblk0p19
- 259       13       2048      mmcblk0p20 /pmsys
- 259       14       1024      mmcblk0p21 /teecfg
+ 259       13       2048      mmcblk0p20      /pmsys
+ 259       14       1024      mmcblk0p21      /teecfg
  259       15       1024      mmcblk0p22
  259       16       4096      mmcblk0p23
- 259       17       5120      mmcblk0p24 /uboot
+ 259       17       5120      mmcblk0p24      /uboot
  259       18       1024      mmcblk0p25
- 259       19       1024      mmcblk0p26 /vbmeta
+ 259       19       1024      mmcblk0p26      /vbmeta
  259       20       1024      mmcblk0p27
  259       21       1024      mmcblk0p28
  259       22       1024      mmcblk0p29
  259       23      16384      mmcblk0p30
- 259       24       6144      mmcblk0p31 /logo
- 259       25       4096      mmcblk0p32 /fbootlogo
+ 259       24       6144      mmcblk0p31      /logo
+ 259       25       4096      mmcblk0p32      /fbootlogo
  259       26      17408      mmcblk0p33
  259       27      10240      mmcblk0p34
- 259       28       1024      mmcblk0p35 /gnssmodem
- 259       29      10240      mmcblk0p36 /wcnmodem
- 259       30      65536      mmcblk0p37 /boot
- 259       31      65536      mmcblk0p38 /recovery
- 259       32       8192      mmcblk0p39 /dtbo
+ 259       28       1024      mmcblk0p35      /gnssmodem
+ 259       29      10240      mmcblk0p36      /wcnmodem
+ 259       30      65536      mmcblk0p37      /boot
+ 259       31      65536      mmcblk0p38      /recovery
+ 259       32       8192      mmcblk0p39      /dtbo
  259       33      76800      mmcblk0p40
  259       34      24576      mmcblk0p41
- 259       35      16384      mmcblk0p42 /metadata
- 259       36       1024      mmcblk0p43 /data
- 259       37       1024      mmcblk0p44 /vbmeta_vendor
+ 259       35      16384      mmcblk0p42      /metadata
+ 259       36       1024      mmcblk0p43      /data
+ 259       37       1024      mmcblk0p44      /vbmeta_vendor
  259       38       6144      mmcblk0p45
  259       39    6582272      mmcblk0p46
  259       40     425984      mmcblk0p47
  259       41      20480      mmcblk0p48
- 259       42     204800      mmcblk0p49 /cache
+ 259       42     204800      mmcblk0p49      /cache
  259       43      24576      mmcblk0p50
  259       44      10240      mmcblk0p51
  259       45       6144      mmcblk0p52
- 259       46   22815724      mmcblk0p53 /userdata
+ 259       46   22815724      mmcblk0p53      /userdata
  179       16       4096      mmcblk0boot1
  179        8       4096      mmcblk0boot0
  179       24   30228480      mmcblk1
